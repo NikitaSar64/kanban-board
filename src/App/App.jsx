38cbea8c91d2eb19e64d46489169dba28bc9f528
Nikita_dev <nikitasar64@gmail.com>
2022-11-07 14:14:05 +0300

    delete start react files, add fonts, set folder location

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskCard from '../Components/TaskCard/TaskCard';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TaskCard/>
+    </div>
+  );
+}
+
+export default App;
