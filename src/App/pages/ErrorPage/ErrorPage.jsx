26fdc8c52ff2678865b3621e8eaed2ff36383ef8
Nikita_dev <nikitasar64@gmail.com>
2022-11-18 13:51:48 +0300

    feat: add responsive layout for different devices

--- src/App/pages/ErrorPage/ErrorPage.jsx
+++ src/App/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../../../Components/Header/Header";
 import './ErrorPage.css';
 
 const ErrorPage = () => {
     return (
+        <>
+        <Header/>
         <div>Page not found</div>
+        </>
     )
 }
 
